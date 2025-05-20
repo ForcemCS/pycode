c40def5edf85557dae61d9e6fda17a81bc8370e0
Kui Wu <kuiinative@gmail.com>
2025-05-20 20:24:25 +0800

    list 推导式的解释

--- iteration/for-3.py
+++ iteration/for-3.py
@@ -38,6 +38,8 @@ count = sum(1 for val in data if val is not None)
 total = sum(val for val in data if val is not None)
 average = total / count
 #列表推导式的写法，用于创建新的list
+#[ A if 条件 else B for 变量 in 列表 ],读作：「如果 条件 成立就用 A，否则用 B，遍历 列表」
+
 data = [val if val is not None else average for val in data]
 print(data)
 
